dafny-lang/dafny

Feature of verification priority ordering
This reverts commit 811783af516a72b11b3297f07781124ee4dcc7c5.

Source/DafnyLanguageServer/Workspace/DafnyDocument.cs
@@ -57,6 +57,12 @@ namespace Microsoft.Dafny.LanguageServer.Workspace {
       Regex.Matches(Text.Text, "\r?\n").Count + 1
     );
 
+    // List of last 5 top-level touched verification diagnostics positions
+    public List<Position> LastTouchedMethodPositions { get; init; } = new();
+
+    // Used to prioritize verification to one method and its dependencies
+    public Range? LastChange { get; set; } = null;
+
     /// <summary>
     /// Checks if the given document uri is pointing to this dafny document.
     /// </summary>

Source/DafnyLanguageServer/Workspace/TextDocumentLoader.cs
@@ -595,12 +595,47 @@ namespace Microsoft.Dafny.LanguageServer.Workspace {
       }
 
       /// <summary>
-      /// Returns the verification priority for a given token.
+      /// Returns the verification priority for a given token, depending on if it's the token
+      /// of a method modified recently (last 5 edits)
       /// </summary>
       /// <param name="token">The token to consider</param>
       /// <returns>The automatically set priority for the underlying method, or 0</returns>
       private int GetVerificationPriority(IToken token) {
+        var lastChange = document.LastChange;
+        if (lastChange == null) {
+          return 0;
+        }
+
+        var implPosition = token.GetLspPosition(); ;
+        // We might want to simplify this quadratic algorithm
+        var method = document.VerificationNodeDiagnostic.Children.FirstOrDefault(node =>
+          node != null && node.Range.Contains(implPosition), null);
+        if (method != null) {
+          if (method.Range.Intersects(lastChange)) {
+            RememberLastTouchedMethod(method);
+            return 10;
+          }
+          // 0 if not found
+          var priority = 1 + document.LastTouchedMethodPositions.IndexOf(method.Position);
+          return priority;
+        }
+        // Can we do the call graph?
         return 0;
+      }
+
+      /// <summary>
+      /// Helper to remember that a method node was recently modified.
+      /// </summary>
+      /// <param name="method">The node diagnostic of the method that was recently modified</param>
+      private void RememberLastTouchedMethod(NodeDiagnostic method) {
+        var index = document.LastTouchedMethodPositions.IndexOf(method.Position);
+        if (index != -1) {
+          document.LastTouchedMethodPositions.RemoveAt(index);
+        }
+        document.LastTouchedMethodPositions.Add(method.Position);
+        while (document.LastTouchedMethodPositions.Count() > 5) {
+          document.LastTouchedMethodPositions.RemoveAt(0);
+        }
       }
 
       /// <summary>

jakenater99/dafny-proof-copy

Source/DafnyLanguageServer/Workspace/DafnyDocument.cs
@@ -57,6 +57,12 @@ namespace Microsoft.Dafny.LanguageServer.Workspace {
       Regex.Matches(Text.Text, "\r?\n").Count + 1
     );
 
+    // List of last 5 top-level touched verification tree positions
+    public List<Position> LastTouchedVerificationTreePositions { get; init; } = new();
+
+    // Used to prioritize verification to one method and its dependencies
+    public Range? LastChange { get; set; } = null;
+
     /// <summary>
     /// Checks if the given document uri is pointing to this dafny document.
     /// </summary>

Source/DafnyLanguageServer/Workspace/TextDocumentLoader.cs
@@ -595,12 +595,47 @@ namespace Microsoft.Dafny.LanguageServer.Workspace {
       }
 
       /// <summary>
-      /// Returns the verification priority for a given token.
+      /// Returns the verification priority for a given token, depending on if it's the token
+      /// of a method modified recently (last 5 edits)
       /// </summary>
       /// <param name="token">The token to consider</param>
       /// <returns>The automatically set priority for the underlying method, or 0</returns>
       private int GetVerificationPriority(IToken token) {
+        var lastChange = document.LastChange;
+        if (lastChange == null) {
+          return 0;
+        }
+
+        var implPosition = token.GetLspPosition(); ;
+        // We might want to simplify this quadratic algorithm
+        var method = document.VerificationTree.Children.FirstOrDefault(node =>
+          node != null && node.Range.Contains(implPosition), null);
+        if (method != null) {
+          if (method.Range.Intersects(lastChange)) {
+            RememberLastTouchedMethod(method);
+            return 10;
+          }
+          // 0 if not found
+          var priority = 1 + document.LastTouchedVerificationTreePositions.IndexOf(method.Position);
+          return priority;
+        }
+        // Can we do the call graph?
         return 0;
+      }
+
+      /// <summary>
+      /// Helper to remember that a method tree was recently modified.
+      /// </summary>
+      /// <param name="method">The verification tree of the method that was recently modified</param>
+      private void RememberLastTouchedMethod(VerificationTree method) {
+        var index = document.LastTouchedVerificationTreePositions.IndexOf(method.Position);
+        if (index != -1) {
+          document.LastTouchedVerificationTreePositions.RemoveAt(index);
+        }
+        document.LastTouchedVerificationTreePositions.Add(method.Position);
+        while (document.LastTouchedVerificationTreePositions.Count() > 5) {
+          document.LastTouchedVerificationTreePositions.RemoveAt(0);
+        }
       }
 
       /// <summary>